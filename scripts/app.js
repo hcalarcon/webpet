792c28a095fb0bc7f83b4720c006c492b30c6908
hcalarcon <hernaldo.alar@gmail.com>
2024-09-26 15:56:55 -0300

    mejoras

--- scripts/app.js
+++ scripts/app.js
@@ -60,3 +60,29 @@ window.addEventListener("scroll", () => {
     navMenu.classList.remove("headerColor");
   }
 });
+
+const fetchData = async () => {
+  const response = await fetch("./data/card.json");
+  const data = await response.json();
+  return data;
+};
+
+const DibujarCard = async () => {
+  const data = await fetchData();
+
+  const cardContainer = document.querySelector(".cardContainer");
+  console.log(cardContainer);
+  data.map((element) => {
+    cardContainer.innerHTML += `
+    <article class="card">
+            <img src="${element.img}" alt="" />
+            <p class="ename">${element.name}</p>
+            <p class="edesc">${element.shortDesc}
+            </p>
+            <a class="verMas" href="${element.url}" target="_blanck">Conocenos</a>
+          </article>
+    `;
+  });
+};
+
+DibujarCard();
